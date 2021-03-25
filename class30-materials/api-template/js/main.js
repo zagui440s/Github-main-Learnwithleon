154f0fca23b37ebe59a1730b73ac18ae353de9ea
Zeus_Aguilar <zaguilar2142@gmail.com>
2021-03-25 02:05:24 -0500

    tenth-try

---
+++ class30-materials/api-template/js/main.js
@@ -0,0 +1,8 @@
+//Make an api request using async await
+
+async function getACuteDogPhoto(){
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await res.json()
+    console.log(data)
+}
+getACuteDogPhoto()
