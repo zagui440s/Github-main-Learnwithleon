223fb28a3335e5b1d3a66de40983fa49ac8a632a
Zeus_Aguilar <zaguilar2142@gmail.com>
2021-04-15 01:16:45 -0500

    code-wars-math-random-solution

---
+++ codewars/code-wars-debugging-math-random/js/main.js
@@ -0,0 +1,11 @@
+function yourFutureCareer(){
+   let career = Math.random();
+		if (career <= 0.32){
+			return 'FrontEnd Developer'
+    }else if (career <= 0.65){
+			return 'BackEnd Developer'
+		}else {
+      return 'Full-Stack Developer'
+    }
+  }
+console.log(yourFutureCareer())
